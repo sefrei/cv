d3d951037e8507dfa6ea1593f7a579b48896c56f
sefrei <secoy@live.com>
2018-06-17 10:48:09 +0200

    initial commit

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(Request $request, \Swift_Mailer $mailer)
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+           $contactFormData = $form->getData();
+           $message = (new \Swift_Message('Un mail de '.$contactFormData['name']))
+               ->setFrom([$contactFormData['email']  => 'Frei Frei'])
+               ->setTo('secoy@live.com')
+               ->setCharset('utf-8')
+               ->setContentType('text/html')
+               ->setBody($this->renderView('email/swift.html.twig',
+                   [
+                       'mail' => $contactFormData,
+                   ]));
+            $message->attach(\Swift_Attachment::fromPath('images/SevCoy.svg')->setFilename('severine.svg'));
+
+           $mailer->send($message);
+
+           if ($mailer->send($message)) {
+              $this->addFlash('success', 'Votre message a bien été envoyé!');
+            } else {
+              $this->addFlash('danger', 'Nous avons rencontré un problème, votre message n\'a pas pu être envoyé');
+            }
+
+           return $this->redirectToRoute('contact');
+       }
+
+        return $this->render('contact/index.html.twig', [
+            'our_form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,86 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+
+  /**
+   *@Route("/", name="index")
+   * @return Response
+   */
+    public function indexAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return $this->render('main/accueil.html.twig', array(
+            'number' => $number,
+        ));
+    }
+
+    /**
+     *@Route("/formations", name="formations")
+     * @return Response
+     */
+      public function formationsAction()
+      {
+          $number = mt_rand(0, 100);
+
+          return $this->render('main/formations.html.twig', array(
+              'number' => $number,
+          ));
+      }
+
+      /**
+       *@Route("/savoir_faire", name="savoir_faire")
+       * @return Response
+       */
+        public function savoirFaireAction()
+        {
+            $number = mt_rand(0, 100);
+
+            return $this->render('main/savoir-faire.html.twig', array(
+                'number' => $number,
+            ));
+        }
+
+        /**
+         *@Route("/print/web", name="print-web")
+         * @return Response
+         */
+          public function printWebAction()
+          {
+              $number = mt_rand(0, 100);
+
+              return $this->render('main/print-web.html.twig', array(
+                  'number' => $number,
+              ));
+          }
+
+          /**
+           *@Route("/autres/expériences", name="autres-experiences")
+           * @return Response
+           */
+            public function otherExpeAction()
+            {
+                $number = mt_rand(0, 100);
+
+                return $this->render('main/autres-experiences.html.twig', array(
+                    'number' => $number,
+                ));
+            }
+
+            /**
+             *@Route("/portfolio", name="portfolio")
+             * @return Response
+             */
+              public function portfolioAction()
+              {
+
+                  return $this->render('main/portfolio.html.twig');
+              }
+}
